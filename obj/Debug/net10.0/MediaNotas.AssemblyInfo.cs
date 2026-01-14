6dba4a5562746aa6313103f308ee18d128360564
Prepara Cursos <vinidayrell1@hotmail.com>
2026-01-14 16:13:55 -0300

    Estudo sobre listas

--- obj/Debug/net10.0/MediaNotas.AssemblyInfo.cs
+++ obj/Debug/net10.0/MediaNotas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MediaNotas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c247be117fa8abfa52594bdbf479c07f29fd3eaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("MediaNotas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MediaNotas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
